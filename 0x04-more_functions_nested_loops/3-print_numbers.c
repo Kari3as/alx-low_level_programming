d40e3ce18584a512d01781144d13c65774738af2
Kari3as <kari3as@gmail.com>
2023-09-15 04:19:25 -0700

    Write a program that prints all possible combinations of two two-digit numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - user defuned func
+ *
+ * Description: prinited digit
+ *
+ * Retrun: void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar("\n");
+}
