28c4376d8fecb3b8409b8ce3ee1b5717ac49c8a1
Kari3as <kari3as@gmail.com>
2023-09-15 14:28:44 -0700

    print no. (_)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - usr defined func
+ *
+ * @n: no. of lines
+ *
+ * Return: void
+*/
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i > 0)
+	{
+		_putchar('_');
+		i--;
+	}
+	_putchar('\n');
+}
