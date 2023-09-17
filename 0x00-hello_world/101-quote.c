bd525c9b0bb521a28cb8edf689c742ec8ee9d995
Kari3as <kari3as@gmail.com>
2023-09-17 11:25:50 -0700

    3rd correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
 * main - Entry point
 *
-* Return: Always 1 (Success)
+* Return: 1
 */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, quo, 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
