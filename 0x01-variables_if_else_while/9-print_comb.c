a0fac70d569478ceb09a914e157910438da48ee6
Kari3as <kari3as@gmail.com>
2023-09-24 15:17:31 -0700

    minor 8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,13 @@ int main(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
+		putchar(i);
+
 		if (i != 57)
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(i);
-		}
+	putchar('\n');
 	return (0);
 }
