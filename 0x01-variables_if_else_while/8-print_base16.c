1bebf8f9ffc1e575155119d56d8ec54539e76278
Kari3as <kari3as@gmail.com>
2023-09-24 14:52:37 -0700

    minor 7

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
-	for (i = 97; i < 103; i++)
+	for (i = 'a'; i <= 'f'; i++)
 	{
 		putchar(i);
 	}
