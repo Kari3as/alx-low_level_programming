e0c63f5a7d2202607377df84d8fc2fef100084dd
Kari3as <kari3as@gmail.com>
2023-09-25 16:44:19 -0700

    strchr func

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - return pointer to first ocuurance of chr at str
+ *
+ * @s: string to search through
+ *
+ * @c: char to find
+ *
+ * Return: pointer to char
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
