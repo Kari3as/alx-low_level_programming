02591c1f5067dfc1922faa450fded0312a2e5a57
Kari3as <kari3as@gmail.com>
2023-09-25 15:42:06 -0700

    memcpy func

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy -  cpy data from src to dest
+ *
+ * @dest: pointer to dest location
+ *
+ * @src: pointer to src location
+ *
+ * @n: nr of memo blck to be copied
+ *
+ * Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (src);
+}
