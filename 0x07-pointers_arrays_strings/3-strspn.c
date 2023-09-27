4f5a1304623b609f414374ba7908b2fce5c9546d
Kari3as <kari3as@gmail.com>
2023-09-26 17:00:39 -0700

    strspn func

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strspn - count byte of initial str segment
+ * _strspn - count byte of intial segment of str
  *
  * @s: pointer to a string
  *
  * @accept: pointer to set of string
  *
- * Retrun: nr of bytes of str seg
+ * Return: nr of bytes of str seg
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	
+
 	for (; *s != '\0'; s++)
 	{
 		char *temp_f = accept;
