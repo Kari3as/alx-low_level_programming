58be7094049d7cb9f90936b1080ef8aac04c0974
Kari3as <kari3as@gmail.com>
2023-09-28 09:02:21 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints str
+ *
+ * @s: pointer to a string
+ *
+ * Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s)
+	}
+
+	ـputchar('\n');
+}
