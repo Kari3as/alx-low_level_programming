82196ee84d8f72432b1cd79af9aaf7b86dae0398
Kari3as <kari3as@gmail.com>
2023-09-29 04:27:00 -0700

    print str in reverse

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,2 @@
-#include "main.h"
 
-/**
- * main - entry point of the program
- *
- * Description: print _putchar
- *
- * Retrun: is zero
- */
-int _putchar(void);
-int main(void)
-{
-	_putchar("_putchar\n");
 
-	return (0);
-}
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 
-	for (; *s != '\0' && *s >= 'a'; s++)
+	for (; *s != '\0'; s++)
 	{
 		char *temp_f = accept;
 
