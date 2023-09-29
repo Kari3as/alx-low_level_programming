17b492aaea41e92079c923e9d92a1f520b8df698
Kari3as <kari3as@gmail.com>
2023-09-29 15:16:17 -0700

    power func

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return x to the power of y
+ *
+ * @x: the basde number
+ *
+ * @y: the exponent
+ *
+ * Return: x  ** y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
