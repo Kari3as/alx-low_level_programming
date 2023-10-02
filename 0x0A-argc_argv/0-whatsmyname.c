b5c9b7fcd8db3770ce057d15592c63382ee532f7
Kari3as <kari3as@gmail.com>
2023-10-02 16:34:47 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - with comand line arguments
+ *
+ * @argc: no. of args
+ *
+ * @argv: arrays contains comd line args
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
