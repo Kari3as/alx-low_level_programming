c038fa0082249bfc485f5a605b0af8dd90010674
Kari3as <kari3as@gmail.com>
2023-10-02 20:09:18 -0700

    addition func

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
 
 /**
- * main -  calc add of 2 nums
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
  *
- * @argc: no. of args
- *
- * @argv: array of args str
- *
- * Return: 0 or 1
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
-{
-	int i;
 
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
 	int sum = 0;
 
-	if (argc == 1)
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
+
 		{
-			if (*argv[i] >= '0' || *argv[i] <= '9')
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
-		printf("%d\n", sum);
+
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
+
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
+
