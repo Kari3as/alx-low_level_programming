ac1e29e10272007c12ba63bdc27df8b418a686a9
Kari3as <kari3as@gmail.com>
2023-10-02 17:30:23 -0700

    print no. of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * main -  passed cmd line args
- *
- * @argc: args count
- *
- * @argv: array of arg strgs
- *
- * Return:0
-*/
 
-int main(int argc, char *argv[] _attribute__((unused)))
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
 {
+(void) argv;
 	printf("%d\n", argc - 1);
 
 	return (0);
