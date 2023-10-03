84bd0635800b3ddd07b14a3102bb91fc500af9b5
Kari3as <kari3as@gmail.com>
2023-10-02 17:07:56 -0700

    print no. of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  passed cmd line args
+ *
+ * @argc: args count
+ *
+ * @argv: array of arg strgs
+ *
+ * Return:0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+
+		return (0);
+
+	}
+}
