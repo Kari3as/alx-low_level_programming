6de9514fd101632270eb917b2b121db6aaaff70d
Kari3as <kari3as@gmail.com>
2023-10-02 17:26:52 -0700

    print no. of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,13 +12,7 @@
 
 int main(int argc, char *argv[] _attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d\n", i);
-
-	}
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
