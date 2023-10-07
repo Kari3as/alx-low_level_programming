fd9ea641225da08a3146b1fc99858051bc47c67e
Kari3as <kari3as@gmail.com>
2023-10-07 16:20:02 -0700

    allocate memo for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,22 +2,42 @@
 #include <stdlib.h>
 
 /**
+ * setmem -  set memory to 0
+ * @s: pointer to allo memo
+ * @c: zero
+ * Return: pointer
+*/
+
+
+char *setmem(char *s, char c)
+{
+	while (*s != 0)
+	{
+		*s = c;
+		s++
+	}
+
+}
+
+/**
  * _calloc - allocate memo for array
  * @nmemb: no. of elements
  * @size: size of each element
  * Return: pointer to alloc memo
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	ptr = malloc(nmemb * size);
 
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
+	if (nmemb == 0 || size == 0 || ptr == NULL)
+	{
 		return (NULL);
+	}
+
+	setmem(ptr, 0);
 
 	return (ptr);
 }
