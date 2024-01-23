99d5ef855353ced462aaebb709caa2ec38fe226a
Kari3as <kari3as@gmail.com>
2024-01-23 15:33:54 -0800

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new struct
+ *
+ * @name : name of dog
+ *
+ *@age: age of dog
+ *
+ * @owner: owner of dog
+ *
+ * Description: Define a new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
