0c1c9d3e1d7fe93b439b5fc80896bc9fbdf9a989
Kari3as <kari3as@gmail.com>
2024-01-24 18:22:02 -0800

    performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
 /**
- * main - entry point
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 98 on incorrect number of arguments,
- * 99 on invalid operator, 100 if trying to divide or modulo by 0
- */
+* main - ...
+* @argc: ...
+* @argv: ...
+*
+* Return: ...
+*/
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-	
-	int (*op_func)(int, int);
-	
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	result = op_func(a, b);
-	
-	printf("%d\n", result);
-	
-	return (0);
+   int (*oprt)(int, int);
+
+
+   if (argc != 4)
+   {
+       printf("Error\n");
+       exit(98);
+   }
+
+
+   oprt = get_op_func(argv[2]);
+
+
+   if (!oprt)
+   {
+       printf("Error\n");
+       exit(99);
+   }
+
+
+   printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+   return (0);
 }
 
